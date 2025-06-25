276158dff0ab74bb6142349f8866480029a80c8e
Saed-Alewaity <sewaity@ucas.edu.ps>
2025-06-25 13:30:34 +0300

    Delete Flight-services/Flight-services directory

--- Flight-services/Flight-services/src/main/java/com/pro/flight/services/repository/FlightRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pro.flight.services.repository;
-
-import com.pro.flight.services.model.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlightRepository extends JpaRepository<Flight,Long> {
-    Flight findFlightById(Long id);
-}
